4cdcf06c27a5f8fa469a32a537740642e892f776
dimascr27 <dimascr27@gmail.com>
2021-03-30 19:31:14 +0700

    Create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '_', app()->getLocale())}}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Completible" content="ie=edge">
+    <title>{{ config('app.name')}}</title>
+</head>
+<body>
+    <div class="jumbotron jumbroton-fluid">
+        <div class="container">
+            <h1 class="display-4">Halaman Home</h1>
+            <p class="lead">Halaman ini Merupakan halaman Home</p>
+        </div>
+    </div>
+</body>
+</html>
