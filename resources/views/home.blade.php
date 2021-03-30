75a58c1ce2c97daa05df6b13921009df455f0b0d
dimascr27 <dimascr27@gmail.com>
2021-03-30 21:15:59 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '_', app()->getLocale())}}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Completible" content="ie=edge">
-    <title>{{ config('app.name')}}</title>
-</head>
-<body>
-    <div class="jumbotron jumbroton-fluid">
+@extends('layout.app')
+
+@section('content')
+    <div class="jumbotron jumbotron-fluide">
         <div class="container">
-            <h1 class="display-4">Halaman Home</h1>
-            <p class="lead">Halaman ini Merupakan halaman Home</p>
+            <h1 class="display-4">Home Page</h1>
+            <p class="lead"> This is the Home Page</p>
         </div>
-    </div>
-</body>
-</html>
+        <p>Nama :{{ $nama }}</p>
+        <p>Mata Pelajaran</p>
+        <ul>
+            @foreach ($collection as $item)
+            <li>{{$p}}</li>    
+            @endforeach
+    </div>    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
 use App\Http\Controllers\ManagementUserController;
+use Illuminate\Routing\Route;
 
 Route::resource('user', ManagementUserController::class);
+
+Route::get("/home", function(){
+    return view("home");
+});
